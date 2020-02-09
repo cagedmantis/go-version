a67cb541a3eb240359509bca3992a5b3d0372af3
Carlos Amedee <carlos@amedee.net>
2020-02-09 12:05:30 -0500

    Remove init function.

--- version.go
+++ version.go
@@ -4,13 +4,9 @@ import (
 	"regexp"
 )
 
-var r *regexp.Regexp
-
-func init() {
-	r = regexp.MustCompile(`^go[1-9][0-9]*(\.[1-9][0-9]*(|(beta|rc)[1-9][0-9]*)|)$`)
-}
+var validator = regexp.MustCompile(`^go[1-9][0-9]*(\.[1-9][0-9]*(|(beta|rc)[1-9][0-9]*)|)$`)
 
 // IsValid returns true if the version of go is a valid version.
 func IsValid(v string) bool {
-	return r.MatchString(v)
+	return validator.MatchString(v)
 }
